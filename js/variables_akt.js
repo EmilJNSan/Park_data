c5734ffae9ebbdd1d0aabfe3205151ba8776237a
EmilJNSan <Emil.Johan.Nyfors@Sandnes.Kommune.no>
2024-07-23 13:14:29 +0200

    Update variables_akt.js

--- js/variables_akt.js
+++ js/variables_akt.js
@@ -1,5 +1,5 @@
-const jsonUrl = "https://Laila72.github.io/Park_data/aktiviteter.json";
-const direct = "https://Laila72.github.io/bhage_kart/";
+const jsonUrl = "json/aktiviteter.json";
+const direct = "https://Laila72.github.io/Park_data/";
 const grunnkart = "https://services.geodataonline.no/arcgis/rest/services/Geocache_WMAS_WGS84/GeocacheBasis/MapServer/tile/{z}/{y}/{x}";
 const attribution_tag = '©️ Geodata AS, Kartverket, Geovekst og kommunene, OpenStreetMap';
 const marker_blaa = "css/images/icon/marker-icon_blaa.png";
