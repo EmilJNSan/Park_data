43b3b5ecfc2f5860b9676400f3e05295c42b4e18
EmilJNSan <Emil.Johan.Nyfors@Sandnes.Kommune.no>
2024-07-23 13:15:31 +0200

    Update variables_bhage.js

--- js/variables_bhage.js
+++ js/variables_bhage.js
@@ -1,4 +1,4 @@
-const jsonUrl = "https://Laila72.github.io/Park_data/bhg.json";
+const jsonUrl = "https://Laila72.github.io/Park_data/json/bhg.json";
 const direct = "https://Laila72.github.io/Park_data/";
 const grunnkart = "https://services.geodataonline.no/arcgis/rest/services/Geocache_WMAS_WGS84/GeocacheBasis/MapServer/tile/{z}/{y}/{x}";
 const attribution_tag = '©️ Geodata AS, Kartverket, Geovekst og kommunene, OpenStreetMap';
