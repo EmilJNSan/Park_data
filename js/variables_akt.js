2edd25fdf25c0ea6b06dd802ab25d2de0c15fb19
EmilJNSan <Emil.Johan.Nyfors@Sandnes.Kommune.no>
2024-07-26 08:35:52 +0200

    Update variables_akt.js

--- js/variables_akt.js
+++ js/variables_akt.js
@@ -4,3 +4,8 @@ const grunnkart = "https://services.geodataonline.no/arcgis/rest/services/Geocac
 const attribution_tag = '©️ Geodata AS, Kartverket, Geovekst og kommunene, OpenStreetMap';
 const marker_blaa = "css/images/icon/marker-icon_blaa.png";
 const testtekst = "Test variabler variant 1";
+
+var blaatikon = L.icon({
+	  iconUrl: `${direct}${marker_blaa}`,
+	  iconSize: [25, 41],
+	  shadowSize: [25, 41]});
